aca1b76692f2111d83532027b0904bd1f43321e7
Jack McCormick <johnmccormick1118@gmail.com>
2019-01-17 17:19:10 -0600

    Added 'About' section mockup and did some touch-ups elsewhere.

--- live-site/src/pages/about.js
+++ live-site/src/pages/about.js
@@ -6,8 +6,14 @@ import aboutStyles from './styles/about.module.scss';
 export default () => (
   <Layout>
     <div className={aboutStyles.container}>
-      <h1>About me</h1>
-      <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+      <div className={aboutStyles.hero}>
+        <h1>Barb is the best in the business.</h1>
+      </div>
+      <div className={aboutStyles.aboutBody}>
+        <h2>I’m good enough, I’m smart enough, and gosh darn it, people like me!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita tempora inventore mollitia quibusdam dolorem nesciunt beatae officiis quod molestias provident odit distinctio at, quis commodi quia. Sed impedit deserunt fuga.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero quidem, consequuntur debitis dolorem, saepe minima odit neque hic veritatis officia voluptatum, soluta error! Earum hic laboriosam nihil ut beatae blanditiis.</p>
+      </div>
 
     </div>
   </Layout>
