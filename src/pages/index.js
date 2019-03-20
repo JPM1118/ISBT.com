cfcef7c70590f3f76156ce06c2e90504a8c087f7
Jack McCormick <johnmccormick1118@gmail.com>
2019-03-20 15:09:47 -0500

    more form changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,12 +12,12 @@ export default () =>
       <div className={indexStyles.bentoForm}>
         <form name="subscribe" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
           <input type="hidden" name="form-name" value="subscribe" />
-          <label htmlFor="subscribe">
-            <h2>Singup for your complimentary Sample Bento Box.</h2>
-            <img className src={bentoImg} alt="bento sample box" />
-            <input type="email" placeholder="email..." />
-            <button type="submit">Submit</button>
+          <h2>Singup for your complimentary Sample Bento Box.</h2>
+          <img className src={bentoImg} alt="bento sample box" />
+          <label htmlFor="email">
+            <input type="email" name="email" placeholder="email..." />
           </label>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
